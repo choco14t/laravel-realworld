991d86b721ee392ea95a077de4ee6bf3f7054d3e
choco <chocoh14t@gmail.com>
2020-03-11 04:33:26 +0900

    comment: remove eloquent docs
    use _ide_helper_models.php

--- app/Eloquents/EloquentUser.php
+++ app/Eloquents/EloquentUser.php
@@ -7,39 +7,6 @@ use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
-/**
- * App\Eloquents\EloquentUser
- *
- * @property int $id
- * @property string $username
- * @property string $email
- * @property string $password
- * @property string|null $bio
- * @property string|null $image
- * @property \Illuminate\Support\Carbon|null $email_verified_at
- * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @property-read int|null $notifications_count
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereBio($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereImage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereUsername($value)
- * @mixin \Eloquent
- * @property-read mixed $token
- * @property string $user_name
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentUser whereUserName($value)
- */
 class EloquentUser extends Authenticatable implements JWTSubject
 {
     use Notifiable;
