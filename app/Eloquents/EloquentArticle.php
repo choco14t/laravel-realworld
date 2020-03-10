efa6dd8a4598317fdea8662b574a716b61441825
choco14t <choco14t@users.noreply.github.com>
2020-03-11 04:33:26 +0900

    comment: remove eloquent docs
    use _ide_helper_models.php

--- app/Eloquents/EloquentArticle.php
+++ app/Eloquents/EloquentArticle.php
@@ -5,30 +5,6 @@ namespace App\Eloquents;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Eloquents\EloquentArticle
- *
- * @property int $id
- * @property int $user_id
- * @property string $slug
- * @property string $title
- * @property string $description
- * @property string $body
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentArticle whereUserId($value)
- * @mixin \Eloquent
- */
 class EloquentArticle extends Model
 {
     protected $table = 'articles';
--- app/Eloquents/EloquentComment.php
+++ app/Eloquents/EloquentComment.php
@@ -4,26 +4,6 @@ namespace App\Eloquents;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Eloquents\EloquentComment
- *
- * @property int $id
- * @property int $user_id
- * @property string $body
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment whereUserId($value)
- * @mixin \Eloquent
- * @property int $article_id
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Eloquents\EloquentComment whereArticleId($value)
- */
 class EloquentComment extends Model
 {
     protected $table = 'comments';
