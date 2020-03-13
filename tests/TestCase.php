8699e73a0b027fd58ee0665d34dcd9807796a298
choco <chocoh14t@gmail.com>
2020-03-13 18:50:38 +0900

    test: refactoring

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,41 @@
 
 namespace Tests;
 
+use App\Eloquents\EloquentUser;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @var EloquentUser
+     */
+    protected $loggedInUser;
+
+    /**
+     * @var EloquentUser
+     */
+    protected $user;
+
+    /**
+     * @var array
+     */
+    protected $headers;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $users = factory(EloquentUser::class, 'test', 2)
+            ->make()
+            ->each(function (EloquentUser $user, int $index) {
+                $user->user_name .= "_{$index}";
+                $user->save();
+            });
+
+        $this->loggedInUser = $users->first();
+        $this->user = $users->last();
+        $this->headers = ['Authorization' => "Token {$this->loggedInUser->token}"];
+    }
 }
