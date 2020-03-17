97a1dc63b0d1d0216528346302a8fb6b186bc5b3
choco14t <choco14t@users.noreply.github.com>
2020-03-17 22:01:53 +0900

    test: post comment to an article
    * HTTP 200 case
    * HTTP 404 case
    * HTTP 422 case
    * HTTP 401 case

--- tests/Feature/PostCommentTest.php
+++ tests/Feature/PostCommentTest.php
@@ -2,10 +2,87 @@
 
 namespace Tests\Feature;
 
+use App\Eloquents\EloquentArticle;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class PostCommentTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function testPostSuccessful()
+    {
+        /** @var EloquentArticle $article */
+        $article = $this->user
+            ->articles()
+            ->save(factory(EloquentArticle::class, 1)->make()->first());
+        $request = [
+            'comment' => [
+                'body' => 'test'
+            ]
+        ];
+
+        $response = $this->postJson("/api/articles/{$article->slug}/comments", $request, $this->headers);
+        $response->assertStatus(200)
+            ->assertJson([
+                'comment' => [
+                    'body' => 'test',
+                    'author' => [
+                        'username' => $this->loggedInUser->user_name,
+                        'bio' => $this->loggedInUser->bio,
+                        'image' => $this->loggedInUser->image,
+                        'following' => false
+                    ]
+                ]
+            ]);
+    }
+
+    public function testReturnErrorsWhenArticleNotFound()
+    {
+        $request = [
+            'comment' => [
+                'body' => 'test'
+            ]
+        ];
+
+        $response = $this->post("/api/articles/undefined/comments", $request, $this->headers);
+        $response->assertStatus(404);
+    }
+
+    public function testReturnErrorsWhenInvalidatedRequest()
+    {
+        /** @var EloquentArticle $article */
+        $article = $this->user
+            ->articles()
+            ->save(factory(EloquentArticle::class, 1)->make()->first());
+        $request = [
+            'comment' => [
+                'body' => ''
+            ]
+        ];
+
+        $response = $this->postJson("/api/articles/{$article->slug}/comments", $request, $this->headers);
+        $response->assertStatus(422)
+            ->assertJson([
+                'errors' => [
+                    'body' => []
+                ]
+            ]);
+    }
+
+    public function testReturnErrorsWhenNotLoggedIn()
+    {
+        /** @var EloquentArticle $article */
+        $article = $this->user
+            ->articles()
+            ->save(factory(EloquentArticle::class, 1)->make()->first());
+        $request = [
+            'comment' => [
+                'body' => 'test'
+            ]
+        ];
+
+        $response = $this->postJson("/api/articles/{$article->slug}/comments", $request);
+        $response->assertStatus(401);
+    }
 }
