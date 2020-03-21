a57f6afa836c31a53437860f9cce5a3ff08dc327
choco14t <choco14t@users.noreply.github.com>
2020-03-21 17:57:04 +0900

    test: added test cases and fixed
    * check `articles` structure
    * fixed case filter by favorited
    * test with limit/offset

--- tests/Feature/GetArticleTest.php
+++ tests/Feature/GetArticleTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Eloquents\EloquentArticle;
 use App\Eloquents\EloquentTag;
+use App\Eloquents\EloquentUser;
+use App\ViewModels\ArticleViewModel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,52 +13,89 @@ class GetArticleTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function testGetAllArticles()
+    {
+        $this->setUpArticles($this->user, $count = 3);
+
+        $response = $this->get('/api/articles');
+        $articles = $this->user->articles()->latest()->get();
+        $expected = $articles->map(function (EloquentArticle $article) {
+            return (new ArticleViewModel($article, null))->withoutKey();
+        });
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'articles' => $expected->all(),
+                'articlesCount' => $count
+            ]);
+    }
+
     public function testFilterByTag()
     {
         /** @var EloquentTag $tag */
         $tag = factory(EloquentTag::class, 1)->create()->first();
 
         /** @var \Illuminate\Database\Eloquent\Collection $articles */
-        $articles = $this->setUpArticles($count = 2);
+        $articles = $this->setUpArticles($this->user, $count = 2);
         $articles->first()->tags()->attach($tag);
 
         $response = $this->get('/api/articles?tag=' . $tag->name);
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'articles' => [],
-            'articlesCount' => 1,
-        ]);
+        $response->assertStatus(200)
+            ->assertJson([
+                'articles' => [],
+                'articlesCount' => 1,
+            ]);
     }
 
     public function testFilterByAuthor()
     {
-        $this->setUpArticles($count = 2);
+        $this->setUpArticles($this->loggedInUser, $count = 2);
+
+        $this->setUpArticles($this->user, $count);
         $response = $this->get('/api/articles?author=' . $this->user->user_name);
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'articles' => [],
-            'articlesCount' => $count,
-        ]);
+        $response->assertStatus(200)
+            ->assertJson([
+                'articles' => [],
+                'articlesCount' => $count,
+            ]);
     }
 
     public function testFilterByFavorited()
     {
-        $this->setUpArticles($count = 2);
-        $response = $this->get('/api/articles?author=' . $this->user->user_name);
+        $articles = $this->setUpArticles($this->loggedInUser, $count = 2);
+        $articles->map(function (EloquentArticle $article) {
+            $article->favorited()->attach($this->user->id);
+        });
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'articles' => [],
-            'articlesCount' => $count,
-        ]);
+        $response = $this->get('/api/articles?favorited=' . $this->user->user_name);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'articles' => [],
+                'articlesCount' => $count,
+            ]);
     }
 
-    private function setUpArticles(int $count): iterable
+    public function testGetArticlesWithLimitAndOffset()
     {
-        return $this->user
-            ->articles()
+        $this->setUpArticles($this->user, $count = 20);
+
+        $limit = 10;
+        $offset = 15;
+        $response = $this->getJson("/api/articles?limit={$limit}&offset={$offset}");
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'articlesCount' => $count
+            ])
+            ->assertJsonCount($count - $offset, 'articles');
+    }
+
+    private function setUpArticles(EloquentUser $user, int $count): iterable
+    {
+        return $user->articles()
             ->saveMany(factory(EloquentArticle::class, $count)->make());
     }
 }
